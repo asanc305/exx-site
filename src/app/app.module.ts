230b65c8a6b9d5679da10ebd2d3f58a58a6b6025
Alex <asanc305@fiu.edu>
2018-03-30 20:11:35 -0400

    export db

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import {MatGridListModule} from '@angular/material/grid-list';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+
+
+
 
 import { AppComponent } from './app.component';
 import { MenuTodayComponent } from './menu-today/menu-today.component';
 import { MenuFullComponent } from './menu-full/menu-full.component';
-import { FoodService } from './food.service'
+import { FoodService } from './food.service';
+import { SidenavComponent } from './sidenav/sidenav.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuTodayComponent,
-    MenuFullComponent
+    MenuFullComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
-    MatGridListModule
+    MatGridListModule,
+    MatSidenavModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [ FoodService ],
   bootstrap: [ AppComponent ]
